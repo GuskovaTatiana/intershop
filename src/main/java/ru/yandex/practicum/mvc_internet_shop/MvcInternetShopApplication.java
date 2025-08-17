66a5bacf796fe2259a1494cac3419387e7fab27f
terokhovets <terokhovets@mdaudit.ru>
2025-08-17 17:27:15 +0300

    spring-mvc. v1. initial project setup

---
+++ src/main/java/ru/yandex/practicum/mvc_internet_shop/MvcInternetShopApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.mvc_internet_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcInternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcInternetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/practicum/mvc_internet_shop/MvcInternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.mvc_internet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcInternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
